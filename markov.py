c77d36bca71c71338a327383251cb11a96778e58
laureli <lk.mallek@gmail.com>
2013-07-07 13:36:01 -0700

    correct script capitalization

--- markov.py
+++ markov.py
@@ -5,8 +5,8 @@ import random
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
-    markov_chains = {}
 
+    markov_chains = {}
     words = corpus.split()
 
     for idx in range(len(words)-2):
@@ -23,11 +23,9 @@ def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
     """
-    Step 1: Pick a key pair to start
-    Step 2: Print key pair
+    Step 1: Pick a key pair to start | Step 2: Print key pair
     Step 3: Choose a value from potential next words
-    Step 4: Print value
-    Step 5: Make key[1] and value in to new key
+    Step 4: Print value | Step 5: Make key[1] and value in to new key
     Step 6: Go to Step 3, repeat until done
     """
     init_key = random.choice(chains.keys())
@@ -36,52 +34,65 @@ def make_text(chains):
 
     markov_text = init_key[0] + " " + init_key[1]
     count = 0
-    while init_key in chains and count < 500:
+    while init_key in chains and count < 800:
         choose_value = random.choice(chains[init_key])
         markov_text = markov_text + " " + choose_value
         init_key = (init_key[1], choose_value)
         count += 1
     
     return markov_text
-    """
+    """ 
     Potential awesome things to do:
-    1: Limit the length of the text
-    2: Make the seed pair make sense as the start of a sentence
-    2a. Make the print out structure more like the play.
+    1: Make the seed pair make sense as the start of a sentence
+    2: Make the print out structure more like the play.
     3: Utilize punctuation
     4: Mashup
     5: Tweet
     """
 
 def make_format(body):
-    """
-    Takes the mass of text and formats it to appear more like a script.Capitalization will determine spacing:
-        words in all caps are set off with new lines b/c are 
-        character names or stage directions
-    # """
-    print type(body)
-
     words = body.split()
-    # upper_words = []
-    # lower_words = []
 
-    for word in words:  # this prints out a list of capital words
-        if word.isupper() and len(word)>2:
+    formatted_list = []
+    formatted_words = ""
+
+    idx = 0
+
+    for word in range(len(words)-2):  # this prints out a list of capital words
+        current_word = words[idx]
+        next_word = words[idx+1]
+
+        current_upper = current_word.isupper() and len(current_word)>1
+            # return boolean values
+        next_upper = next_word.isupper() and len(next_word)>1
+
+        # different_case = (current_upper and not next_upper) or (next_upper and not current_upper)
+
+        # if (current_upper and next_upper) or (current_upper not and next_upper):
+
+        formatted_list.append(current_word)
+        if current_upper and not next_upper:
+            formatted_list.append(":\n\n")
+        elif next_upper and not current_upper:
+            # # check last character of current word --
+            # if next_upper[-1] == ord()
+            # # if last character is not punctuation, insert some punctuation
+            formatted_list.append(".\n\n")
+        formatted_list.append(" ")            # print "first option"
+
+        idx +=1
+        
+
+#this takes the list and makes it a string.
+    for word in formatted_list:
+        # print "formatted list print out"
+        formatted_words += word.format(formatted_words)
+
             # print word.index
-            print word
+    print "the string is ", formatted_words
+    # print "the list is ", formatted_list
 
-    # print words
 
-    """
-    Step 1: Access word in all caps  -- okay
-    Step 2: Print words in all caps  -- okay
-    Step 3: Add 2 new lines above (1 empty line) and 1 below
-        --> this means making a new string?
-    Step 4: Combine capital words with un-capitalized
-
-    ISSUES: What if multiple capitalized words follow each other?
-            What about I, O, A, 
-    """
 
 
 def main():
